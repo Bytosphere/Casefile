b07a101f0d2be2b898f802a3962c1e98beffbcb9
Bytosphere <bytosphere.dev@proton.me>
2026-08-20 14:13:47 +0300

    bump to v0.3.0

--- internal/command/root.go
+++ internal/command/root.go
@@ -5,7 +5,7 @@ import (
 )
 
 // version is the main version of the CLI.
-var version = "0.2.0"
+var version = "0.3.0"
 
 // rootCmd is the command that all subcommands belong to.
 var rootCmd = &cobra.Command{
